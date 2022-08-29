3193cfac1fa3ef04a3120d128ffe01b599503e37
Esteban Perez <moxepz@gmail.com>
2022-08-29 18:04:43 -0500

    creacion del repositorio

---
+++ Taller_poo/Actividad1/Entidad.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Entidad
+    {
+        public  string Name { get; protected set; }
+        protected Entidad(string Name)
+        {
+            this.Name = Name;
+
+        }
+
+    }
+}
---
+++ Taller_poo/Actividad1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+    using ConsoleApp1;
+    Persona P = new Persona("juan", "gomez");
+    Animal a = new Animal("perro", "can");
+
+
+
+    Console.WriteLine(P.Name);
+
