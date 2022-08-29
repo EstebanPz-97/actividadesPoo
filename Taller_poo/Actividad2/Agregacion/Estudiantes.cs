9b536081246fd496a2f981b4d68ee89dcd03c640
Esteban Perez <moxepz@gmail.com>
2022-08-29 18:12:42 -0500

    Actividad 2

---
+++ Taller_poo/Actividad2/Agregacion/Estudiantes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabajo2.Agregacion
+{
+    internal class Estudiantes
+    {
+        private Escuela Escuela;
+
+        public Estudiantes(Escuela escuela)
+        {
+            this.Escuela = escuela;
+        }
+    } 
+}
---
+++ Taller_poo/Actividad2/entities1/VehiculoElectrico1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabajo2.entities1
+{
+    internal class VehiculoElectrico1:Entity1
+    {
+        public override void Arrancar()
+        {
+            //algoritmo
+            Console.WriteLine("Arrancar un vehiculo electrico");
+        }
+    }
+}
