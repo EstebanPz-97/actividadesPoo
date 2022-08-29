a7db122fb80d9b801b49f37679d9f204b2826b52
Esteban Perez <moxepz@gmail.com>
2022-08-29 18:15:37 -0500

    Actividad 4

---
+++ Taller_poo/Actividad4/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using ConsoleApp2.Fabrica;
+using ConsoleApp2.entities;
+
+Ifactory Factory = new Factory();
+
+
+Factory.BuildGame(1, "fifa ", "ps5");
+Factory.BulidConsole(1, "ps5", "1tb", 25000000);
+
+var game = (game)Factory.BuildGame(2, "god of gard ", "pc");
+
+var console = (console)Factory.BulidConsole(2, "nintendo", "250gb", 1500000);
+Console.WriteLine(game);
+Console.WriteLine(console);
---
+++ Taller_poo/Actividad4/entities/game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.entities
+{
+    internal class game : entity
+    {
+        public string plataform { get; private set; }
+        public game(int id, string name, string plataform) : base(id, name)
+        {
+            plataform = plataform;
+        }
+    }
+}
